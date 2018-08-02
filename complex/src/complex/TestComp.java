20525f6aa5c271330bd04ff157ab39bc6a7a27fe
rakrao <41562055+rakrao@users.noreply.github.com>
2018-08-02 23:39:01 +0530

    Add files via upload

--- complex/src/complex/TestComp.java
+++ complex/src/complex/TestComp.java
@@ -5,32 +5,32 @@ import java.util.Scanner;
 
 public class TestComp {
 	public static void main(String[] args) {
-		                                          //main function
-		
-		comp Comp1=new comp();
-		comp Comp2=new comp();  //creating objects
-		comp Comp3=new comp();
-		
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter real part of first number");
-Comp1.real=sc.nextFloat();
+		// main function
 
-System.out.println("Enter imaginary part of first number");
-Comp1.imaginary=sc.nextFloat();
+		comp Comp1 = new comp();
+		comp Comp2 = new comp(); // creating objects
+		comp Comp3 = new comp();
 
-System.out.println("Enter real part of second number");
-Comp2.real=sc.nextFloat();
+		Scanner sc = new Scanner(System.in); // scanner input
+		System.out.println("Enter real part of first number");
+		Comp1.real = sc.nextFloat();
 
-System.out.println("Enter imaginary part of first number");
-Comp2.imaginary=sc.nextFloat();
+		System.out.println("Enter imaginary part of first number");
+		Comp1.imaginary = sc.nextFloat();
 
-float finalreal = Comp1.add(Comp1.real,Comp2.real); 
-float finalimaginary = Comp1.add(Comp1.imaginary,Comp2.imaginary);
+		System.out.println("Enter real part of second number");
+		Comp2.real = sc.nextFloat();
 
-System.out.println("real="+finalreal);
-System.out.println("imaginary="+finalimaginary);
+		System.out.println("Enter imaginary part of first number");
+		Comp2.imaginary = sc.nextFloat();
 
-float finalc=Comp3.add(finalreal,finalimaginary);
-System.out.println("result="+finalc);
-}
+		float finalreal = Comp1.add(Comp1.real, Comp2.real);
+		float finalimaginary = Comp1.add(Comp1.imaginary, Comp2.imaginary);
+
+		System.out.println("real=" + finalreal);
+		System.out.println("imaginary=" + finalimaginary); // printing values
+
+		float finalc = Comp3.add(finalreal, finalimaginary);
+		System.out.println("result=" + finalc);
+	}
 }
--- complex/src/complex/comp.java
+++ complex/src/complex/comp.java
@@ -1,23 +1,26 @@
+//program about complex numbers
 package complex;
 
-public class comp {  //class definition
+public class comp { // class definition
 	float real;
 	float imaginary;
-	
-	public float set(float x,float y)  //set function
+
+	public float set(float x, float y) // set function
 	{
-		real=x;
-		imaginary=y;
-		
-		return x+y;
+		real = x;
+		imaginary = y;
+
+		return x + y;
 	}
-	void display()  //display function
-	{   
-		System.out.println("real="+real);
-		System.out.println("imaginary="+imaginary);
+
+	void display() // display function
+	{
+		System.out.println("real=" + real);
+		System.out.println("imaginary=" + imaginary);
 	}
-public float add(float a, float b)  //add function
-{
-	return a+b;
-}
+
+	public float add(float a, float b) // add function
+	{
+		return a + b;
+	}
 }
